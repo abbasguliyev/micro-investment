abe6f02a2577a59701bea85c5cf1b9478f5d92e7
abbasguliyev <abbasglyev@gmail.com.>
2024-11-13 08:04:29 +0400

    test

--- micro-investment-frontend/src/components/Footer/index.jsx
+++ micro-investment-frontend/src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className={`w-full flex justify-center items-center footer p-5 text-xs md:text-base`}>© 2023 <a href="https://t.me/halalekosistem1" className='ml-2' target='_blank'>Halal Ekosistem</a>, Bütün hüquqlar qorunur.</footer>
+    <footer className={`w-full flex justify-center items-center footer p-5 text-xs md:text-base`}>© 2024 Abbas Guliyev, Bütün hüquqlar qorunur.</footer>
   )
 }
 
